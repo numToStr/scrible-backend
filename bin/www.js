b8b1b2623a780133eb030e854be8cfa9391afc93
vkasraj <vikasraj1911@gmail.com>
2019-01-12 12:03:26 +0530

    app: added mongodb

--- bin/www.js
+++ bin/www.js
@@ -1,13 +1,26 @@
 #!/usr/bin/env node
 
 const http = require("http");
+const mongoose = require("mongoose");
 const app = require("../server");
 
-const { PORT } = require("../config/env.keys");
+const { PORT, MONGO_URI } = require("../config/env.keys");
 
 const server = http.createServer(app);
 
-server.listen(PORT);
+mongoose.connect(
+    MONGO_URI,
+    { useNewUrlParser: true },
+    error => {
+        if (error) {
+            throw new Error(`[MongoDB]::ERROR:${error.message}`);
+        }
+
+        console.log(`[MongoDB]::LISTEN`);
+
+        server.listen(PORT);
+    }
+);
 
 server.on("listening", () => {
     console.log(`[SERVER]::LISTEN:${PORT}`);
--- config/env.keys.js
+++ config/env.keys.js
@@ -1,4 +1,5 @@
 module.exports = {
     NODE_ENV: process.env.NODE_ENV,
-    PORT: process.env.PORT
+    PORT: process.env.PORT,
+    MONGO_URI: process.env.MONGO_URI
 };
