10f29dae0e893d48fd3a452d5c376862029f16e0
vkasraj <vikasraj1911@gmail.com>
2019-01-12 12:10:21 +0530

    www: added some comments

--- bin/www.js
+++ bin/www.js
@@ -2,23 +2,29 @@
 
 const http = require("http");
 const mongoose = require("mongoose");
-const app = require("../server");
 
 const { PORT, MONGO_URI } = require("../config/env.keys");
 
+// Importing Express App
+const app = require("../server");
+
+// Creating HTTP Server
 const server = http.createServer(app);
 
+// Connecting MongoDB ============
 mongoose.connect(
     MONGO_URI,
     { useNewUrlParser: true },
     error => {
+        // if mongoDB is unable to connect
+        // throws error => shutdown app
         if (error) {
             throw new Error(`[MongoDB]::ERROR:${error.message}`);
         }
 
         console.log(`[MongoDB]::LISTEN`);
 
-        // Firing up the server on selected port
+        // Connecting server selected port ========
         server.listen(PORT);
     }
 );
@@ -30,7 +36,7 @@ server.on("listening", () => {
 
 // Function for checking connecting or error
 server.on("error", ({ message }) => {
-    console.log(`[SERVER]::ERROR:${message}`);
+    throw new Error(`[SERVER]::ERROR:${message}`);
 });
 
 /**
