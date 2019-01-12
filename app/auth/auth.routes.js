dbb6283e8f36ba810fa445cc5230b5432eea7fd4
vkasraj <vikasraj1911@gmail.com>
2019-01-12 15:14:05 +0530

    auth: added request validation

--- app/auth/auth.routes.js
+++ app/auth/auth.routes.js
@@ -1,8 +1,11 @@
 const router = require("express").Router();
 
+const $validator = require("../../middlewares/request.validator");
+
+const { signupSchema, loginSchema } = require("./auth.validation");
 const { authSignup, authLogin } = require("./auth.controller");
 
-router.post("/signup", authSignup);
-router.post("/login", authLogin);
+router.post("/signup", $validator(signupSchema), authSignup);
+router.post("/login", $validator(loginSchema), authLogin);
 
 module.exports = router;
